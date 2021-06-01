64a588a6d4d6a4cdd1c8f39b2ff7d5318b0e94e1
ahmedjadan <a.jadan7@gmail.com>
2021-06-01 12:57:26 +0200

    fix twitter card

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { useRouter } from 'next/router';
+
 import { GA_TRACKING_ID } from "../lib/gtag";
 class MyDocument extends Document {
+  
   render() {
+    const router = useRouter();
     return (
       <Html lang="ar">
         <Head>
@@ -41,19 +45,23 @@ class MyDocument extends Document {
             name="keywords"
             content="كورونا، كوفيد19، فيروس كورونا التاجي، حالات كورونا في العالم العربي ، حالات كورونا في اليمن، السعودية، مصر، الامارات، قطر، البحرين، الكويت، السودان، العراق، سوريا، فلسطين، المغرب العربي، موريتانيا، ليبيا"
           />
+          <link rel="canonical" href={`https://corona-ar.vercel.app${router.asPath}`} />
+          <meta key="image" property="image" content="https://corona-ar.vercel.app/socialmediameta.png" />
+          <meta property="og:title" content="منصة كورونا بالعربي" />
+          <meta property="og:description" content="تابع آخر احصاءات فيروس كرونا لحظة بلحظة"/>
+          <meta property="og:image" content="https://corona-ar.vercel.app/socialmediameta.png" />
+
+          <meta property="og:image" content="https://corona-ar.vercel.app/socialmediameta.png" />
           <meta property="og:url" content="https://corona-ar.vercel.app" />
-          <meta property="og:image" content="/socialmediameta.png" />
+          <meta property="og:url" content={`https://corona-ar.vercel.app${router.asPath}`} />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="website" />
           <meta property="twitter:domain" content="corona-ar.vercel.app" />
-
+          <meta key="twitter:image" name="twitter:image" content="https://corona-ar.vercel.app/socialmediameta.png" />
+      <meta property="twitter:image"  content="https://corona-ar.vercel.app/socialmediameta.png" /> 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@a7medjadan" />
           <meta name="twitter:creator" content="@a7medjadan" />
-          <meta property="og:url" content="https://corona-ar.vercel.app/" />
-          <meta property="og:title" content="منصة كورونا بالعربي" />
-          <meta property="og:description" content="تابع آخر احصاءات فيروس كرونا لحظة بلحظة"/>
-          <meta property="og:image" content="/socialmediameta.png" />
         </Head>
         <body>
           <Main />
