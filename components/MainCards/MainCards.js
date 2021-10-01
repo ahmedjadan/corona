ecaccc96626a89e2b94a567912f94108b0f81e8f
ahmedjadan <a.jadan7@gmail.com>
2021-10-01 15:17:42 +0200

    new dark theme

--- components/MainCards/MainCards.js
+++ components/MainCards/MainCards.js
@@ -20,9 +20,12 @@ export default function MainCards({ mainData }) {
           {cases && cases.toLocaleString()}
         </div>
         <div className={styles.card__new_value}>
-          <svg xmlns="http://www.w3.org/2000/svg" className={styles.arrow_up} width="25" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
-          </svg>          {Math.abs(todayCases) > 999
+
+          <svg xmlns="http://www.w3.org/2000/svg" width="28" className={styles.arrow_up} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M18 15l-6 -6l-6 6h12" />
+          </svg>
+          {Math.abs(todayCases) > 999
             ? Math.sign(todayCases) * (Math.abs(todayCases) / 1000).toFixed(1) +
             "k"
             : Math.sign(todayCases) * Math.abs(todayCases)}
@@ -35,8 +38,9 @@ export default function MainCards({ mainData }) {
 
         </div>
         <div className={styles.card__new_value}>
-          <svg xmlns="http://www.w3.org/2000/svg" className={styles.arrow_up} width="25" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="28" className={styles.arrow_up} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M18 15l-6 -6l-6 6h12" />
           </svg>
           {Math.abs(todayDeaths) > 999
             ? Math.sign(todayDeaths) *
@@ -51,8 +55,9 @@ export default function MainCards({ mainData }) {
           {recovered.toLocaleString()}
         </div>
         <div className={styles.card__new_value}>
-          <svg xmlns="http://www.w3.org/2000/svg" className={styles.arrow_up} width="25" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="28" className={styles.arrow_up} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M18 15l-6 -6l-6 6h12" />
           </svg>
           {Math.abs(todayRecovered) > 999
             ? Math.sign(todayRecovered) *
