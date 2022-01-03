bdacf15d4a091a3520e1001fb574af0ffc110fc8
Ahmed Jadan <a.jadan7@gmail.com>
2022-01-04 00:47:06 +0200

    upgrade to next12

--- next.config.js
+++ next.config.js
@@ -1,18 +1,36 @@
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-    }
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-    return config;
+module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    domains: ['disease.sh'],
   },
+  future: { webpack5: true },
+
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
+    runtimeCaching,
   },
-  images: {
-    domains: ['disease.sh'],
-  },
-};
+});
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     if (isServer) {
+//       require('./scripts/generate-sitemap');
+//     }
+
+//     return config;
+//   },
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === 'development',
+//   },
+//   images: {
+//     domains: ['disease.sh'],
+//   },
+// };
