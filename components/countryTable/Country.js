eabac9aca9eb0dcae33d4be7607e55eeb38ede08
ahmedjadan <a.jadan7@gmail.com>
2022-02-21 12:44:07 +0200

    fix react-window rendering bug

--- components/countryTable/Country.js
+++ components/countryTable/Country.js
@@ -1,64 +1,60 @@
 import Link from 'next/link';
 import styles from './countryTable.module.css';
 
-const Country = ({ country }) => {
+const Country = ({ country, todayDeaths,recovered,deaths,todayCases,cases }) => {
+
   return (
     <Link
-      href={`/country/${country.country
+      href={`/country/${country
         .normalize('NFD')
         .replace(/[\u0300-\u036f]/g, '')}`}
     >
       <div className={styles.country__row}>
-        
-        
-        
-        
         <div className={styles.country__recovered}>
-          {Math.abs(country.recovered) > 999999
-            ? Math.sign(country.recovered) *
-                (Math.abs(country.recovered) / 1000000).toFixed(2) +
+          {Math.abs(recovered) > 999999
+            ? Math.sign(recovered) *
+                (Math.abs(recovered) / 1000000).toFixed(2) +
               'M'
-            : country.recovered.toLocaleString()}
+            : recovered.toLocaleString()}
         </div>
         <div className={styles.country__today__deaths}>
           <div
             className={`${styles.country__today__deaths}  ${
-              country.todayDeaths ? styles.new__deaths : ''
+              todayDeaths ? styles.new__deaths : ''
             } `}
           >
-            {country.todayDeaths.toLocaleString()}{' '}
+            {todayDeaths.toLocaleString()}{' '}
           </div>
         </div>
         <div className={styles.country__deaths}>
-          {Math.abs(country.deaths) > 999999
-            ? Math.sign(country.deaths) *
-                (Math.abs(country.deaths) / 1000000).toFixed(3) +
+          {Math.abs(deaths) > 999999
+            ? Math.sign(deaths) *
+                (Math.abs(deaths) / 1000000).toFixed(3) +
               'M'
-            : country.deaths.toLocaleString()}
+            : deaths.toLocaleString()}
         </div>
         <div className={styles.country__new}>
           <div
             className={`${styles.country__new}  ${
-              country.todayCases ? styles.new__cases : ''
+             todayCases ? styles.new__cases : ''
             } `}
           >
-            {Math.abs(country.todayCases) > 999999
-              ? Math.sign(country.todayCases) *
-                  (Math.abs(country.todayCases) / 1000000).toFixed(3) +
+            {Math.abs(todayCases) > 999999
+              ? Math.sign(todayCases) *
+                  (Math.abs(todayCases) / 1000000).toFixed(3) +
                 'M'
-              : country.todayCases.toLocaleString()}
+              :todayCases.toLocaleString()}
           </div>
         </div>
         <div className={styles.country__cases}>
           {' '}
-          {Math.abs(country.cases) > 999999
-            ? Math.sign(country.cases) *
-                (Math.abs(country.cases) / 1000000).toFixed(3) +
+          {Math.abs(cases) > 999999
+            ? Math.sign(cases) *
+                (Math.abs(cases) / 1000000).toFixed(3) +
               'M'
-            : country.cases.toLocaleString()}
+            : cases.toLocaleString()}
         </div>
-        <div className={styles.country__name}> {country.country} </div>
-
+        <div className={styles.country__name}> {country} </div>
       </div>
     </Link>
   );
